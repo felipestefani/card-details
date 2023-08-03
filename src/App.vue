f6cf3b13058899d64501ccfdc384e7bf20c26ede
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-03 11:52:47 -0300

    scratch

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+   0000 0000 0000 0000
+  Jane Appleseed
+  00/00
+
+  000
+
+  Cardholder Name
+  e.g. Jane Appleseed
+
+  Card Number
+  e.g. 1234 5678 9123 0000
+
+  Exp. Date (MM/YY)
+  MM
+  YY
+
+  CVC
+  e.g. 123
+
+  Confirm
+
+  <!-- Completed state start -->
+
+  Thank you!
+  We've added your card details
+  Continue
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+</template>
+
+<style scoped>
+    .attribution { font-size: 11px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+</style>
